26df7787d4afc975294d3890f5430231f016515a
Mateo Miño <118315392+Mino-Mateo@users.noreply.github.com>
2023-06-09 12:22:09 -0500

    Update and rename Main.java to Menu.java

--- Trabajo Clase - Grupo 2/src/Menu.java
+++ Trabajo Clase - Grupo 2/src/Menu.java
@@ -1,6 +1,6 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-public class Main {
+public class Menu {
     public static void main(String[] args) {
         Parabola_Equipo2 parabola = new Parabola_Equipo2(0,0,0,0,0);
         parabola.Parabola_Equipo2_VerticeX(2,2);
