04f6908c7ef2d7e64e0545ac2f7eb40357e680e0
Mateo <minora.mateo@gmail.com>
2023-06-09 12:13:29 -0500

    no message

--- Trabajo Clase - Grupo 2/src/Main.java
+++ Trabajo Clase - Grupo 2/src/Main.java
@@ -2,16 +2,9 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Intro with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Mayús+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        Parabola_Equipo2 parabola = new Parabola_Equipo2(0,0,0,0,0);
+        parabola.Parabola_Equipo2_VerticeX(2,2);
+        parabola.Parabola_Equipo2_VerticeY(2,2,5);
+        parabola.Parabola_Equipo2_ImpVer();
     }
-}
+    }
